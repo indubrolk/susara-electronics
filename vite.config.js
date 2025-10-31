a141b48e2eca7130c7fda8c3124139ef8036be73
indubrolk <110773586+indubrolk@users.noreply.github.com>
2025-10-31 10:00:18 +0530

    gh-pages setuped

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+// Set `base` for GitHub Pages so assets resolve under /susara-electronics/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  base: '/susara-electronics/',
+  plugins: [react(), tailwindcss()],
 })
